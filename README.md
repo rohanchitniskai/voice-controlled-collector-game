956cd18a488815fec7ab38551c24e07e49b3d408
Rohan Chitnis <rohan.chitnis@konverge.ai>
2023-01-25 18:30:13 +0530

    scoreboard implemented

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# voice-controlled-collector-game
+## The App UI
+
+The App UI contains the following components - 
+1.Main page contains the grid by default 10\*10.
+2.Grid has a collector placed at random position.
+3.Grid has mines 1/8th of the grid spots randomly placed.
+4.Dropdown option to change grid size.
+5.Dropdown option to change collector speed.
+6.Connect button to connect with voice API and websocket.
+7.Restart button to restart the game.
+8.Timer to show time elapsed of gameplay.
+9.Table to show scoreboard with grid size and time
+
+## How to Play
+1.Click on the connect button to connect with voice API and websocket
+2.Once connected give the following voice commands - 
+    - "go" to move collector
+    - "stop" to stop collector
+    - "left" to move collector left
+    - "right" to move collector right
+    - "up" to move collector up
+    - "down" to move collector
+3.Once all the mines are picked, grid size and time will be recorded.
+4.Best score will be displayed at the top of the table.
+5.Click on restart button to restart game.
+
+## How to Run
+
+Run command npm install in project directory to install dependencies
+To start server, run command npm run start while in project directory
+
+## The Files
+
+package.json - dependencies required for npm install
+Home.jsx - component creating UI for the app containing the Grid, buttons and methods.
+Home.css- styles for Home.jsx component
+audio-service.js - file contains web socket audio service
+microphone-service.js - file contains microphone voice capture service
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,10 +6,16 @@ import "./Home.css";
 const Home = () => {
   /* state to set grid */
   const [grid, setGrid] = useState();
+  const [gridRows, setGridRows] = useState(10);
+  const [gridCols, setGridCols] = useState(10);
+
+  /* collector speed default set to 1000ms */
+  const [collectorSpeed, setCollectorSpeed] = useState(1000);
 
   /* state to set collector direction */
   const [collectorPosition, setCollectorPosition] = useState("left");
 
+  /* voice command state */
   const [collectorCommand, setCollectorCommand] = useState(collectorPosition);
 
   /* state to set collector co-ordinates in grid */
@@ -17,25 +23,58 @@ const Home = () => {
     xCord: 0,
     yCord: 5,
   });
+
+  // The state for our timer
+  const [timer, setTimer] = useState("00:00:00");
+  const [score, setScore] = useState([]);
+
   /* running state of a grid simulation */
   const [running, setRunning] = useState(false);
   const runningRef = useRef(running);
   runningRef.current = running;
   let timeOutId = null;
   const positionRef = useRef(collectorPosition);
+  let scoreBoard = [];
+  let scoreboardTime = {};
 
-  /* number of rows in grid */
-  const numRows = 10;
-
-  /* number of columns in grid */
-  const numCols = 10;
-
+  const timerRef = useRef(null);
   const [connection, setConnection] = useState(false);
 
   useEffect(() => {
     /* initialize the grid */
     setGrid(initializeGrid());
-  }, []);
+  }, [gridRows, gridCols]);
+
+  /* initialize the grid with collector position */
+  const initializeGrid = () => {
+    const xCord = 0;
+    const yCord = 5;
+    const rows = [];
+    for (let i = 0; i < gridRows; i++) {
+      rows.push(
+        Array.from(Array(gridCols), (ele, colIndex) =>
+          i === xCord && colIndex === yCord ? 2 : 0
+        )
+      );
+    }
+    return initializeMines(rows);
+  };
+
+  /* initialize the grid with mines 1/8th of the grid spots */
+  const initializeMines = (rows) => {
+    const numberOfMines = parseInt((gridRows * gridCols) / 8);
+    let minesCount = 0;
+    /* populate mines at random positions */
+    while (minesCount < numberOfMines) {
+      const x = Math.floor(Math.random() * rows.length);
+      const y = Math.floor(Math.random() * rows[0].length);
+      if (rows[x][y] === 0) {
+        rows[x][y] = 1;
+        minesCount++;
+      }
+    }
+    return rows;
+  };
 
   /*call methods if position changes */
   useEffect(() => {
@@ -52,48 +91,18 @@ const Home = () => {
     }
   }, [collectorPosition]);
 
+  /* check voice command change */
   useEffect(() => {
     if (collectorCommand === "go" || collectorCommand === "stop") {
+      if (collectorCommand === "stop") {
+        if (timerRef.current) {
+          clearInterval(timerRef.current);
+          timerRef.current = null;
+        }
+      }
       startGame();
     }
   }, [collectorCommand]);
-
-  useEffect(() => {
-    if (connection) connectWebsocket();
-  }, [connection]);
-
-  /* initialize the grid with collector position */
-  const initializeGrid = () => {
-    const rows = [];
-    for (let i = 0; i < numRows; i++) {
-      rows.push(
-        Array.from(Array(numCols), (ele, colIndex) =>
-          i === collectorCoordinates.xCord &&
-          colIndex === collectorCoordinates.yCord
-            ? 2
-            : 0
-        )
-      );
-    }
-    return initializeMines(rows);
-  };
-
-  /* initialize the grid with mines 1/8th of the grid spots */
-  //TODO: fix random positions
-  const initializeMines = (rows) => {
-    const numberOfMines = parseInt((numRows * numCols) / 8);
-    let minesCount = 0;
-    /* populate mines at random positions */
-    while (minesCount < numberOfMines) {
-      const x = Math.floor(Math.random() * rows.length);
-      const y = Math.floor(Math.random() * rows[0].length);
-      if (rows[x][y] === 0) {
-        rows[x][y] = 1;
-        minesCount++;
-      }
-    }
-    return rows;
-  };
 
   /* set collector direction on UI */
   const getCollectorDirection = () => {
@@ -114,7 +123,9 @@ const Home = () => {
   const startGame = () => {
     setRunning(!running);
     if (!running) {
+      if (!timerRef.current) startTimer();
       runningRef.current = true;
+      // Ref.current = id;
       if (collectorPosition === "right") {
         moveCollectorRight();
       } else if (collectorPosition === "left") {
@@ -129,6 +140,72 @@ const Home = () => {
     }
   };
 
+  /* reset game  */
+  const restart = () => {
+    // setRunning(false);
+    setCollectorCommand("stop");
+    if (timerRef.current) {
+      clearInterval(timerRef.current);
+      timerRef.current = null;
+    }
+    setTimer("00:00:00");
+    scoreboardTime = {};
+    // /* initialize the grid */
+    setTimeout(() => {
+      setCollectorCoordinates({
+        xCord: 0,
+        yCord: 5,
+      });
+      setGrid(initializeGrid());
+    }, 1000);
+    setCollectorPosition("left");
+  };
+
+  /* start timer */
+  const startTimer = () => {
+    const startTime = new Date();
+    timerRef.current = setInterval(() => {
+      const timers = getElapsedTime(startTime);
+      setTimer(timers.time);
+      scoreboardTime = { time: timers.time, timestamp: timers.timestamp };
+    }, 1000);
+  };
+
+  /* get time elapsed */
+  const getElapsedTime = (startTime) => {
+    let endTime = new Date();
+    let timeDiff = endTime.getTime() - startTime.getTime();
+    const timestamp = timeDiff;
+
+    timeDiff = timeDiff / 1000;
+    let seconds = Math.floor(timeDiff % 60);
+    let secondsAsString = seconds < 10 ? "0" + seconds : seconds + "";
+    timeDiff = Math.floor(timeDiff / 60);
+    let minutes = timeDiff % 60;
+    let minutesAsString = minutes < 10 ? "0" + minutes : minutes + "";
+    timeDiff = Math.floor(timeDiff / 60);
+    let hours = timeDiff % 24;
+    timeDiff = Math.floor(timeDiff / 24);
+    let days = timeDiff;
+
+    let totalHours = hours + days * 24; // add days to hours
+    let totalHoursAsString =
+      totalHours < 10 ? "0" + totalHours : totalHours + "";
+
+    if (totalHoursAsString === "00") {
+      return {
+        time: "00" + ":" + minutesAsString + ":" + secondsAsString,
+        timestamp: timestamp,
+      };
+    } else {
+      return {
+        time:
+          totalHoursAsString + ":" + minutesAsString + ":" + secondsAsString,
+        timestamp: timestamp,
+      };
+    }
+  };
+
   /* move collector right on the grid */
   const moveCollectorRight = async () => {
     positionRef.current = "right";
@@ -136,8 +213,8 @@ const Home = () => {
       let gridCopy = JSON.parse(JSON.stringify(grid));
       /* traverse grid left to right */
       let y = collectorCoordinates.yCord;
-      for (let i = collectorCoordinates.xCord; i < numRows; i++) {
-        for (let j = y; j < numCols; j++) {
+      for (let i = collectorCoordinates.xCord; i < gridRows; i++) {
+        for (let j = y; j < gridCols; j++) {
           /* stop loop if running state change or direction changes  */
           if (!runningRef.current || positionRef.current !== "right") {
             setCollectorPosition(positionRef.current);
@@ -161,7 +238,7 @@ const Home = () => {
       let gridCopy = JSON.parse(JSON.stringify(grid));
       /* traverse grid right to left */
       let y = collectorCoordinates.yCord;
-      for (let i = collectorCoordinates.xCord; i < numRows; i++) {
+      for (let i = collectorCoordinates.xCord; i < gridRows; i++) {
         for (let j = y; j >= 0; j--) {
           /* stop loop if running state change or direction changes  */
           if (!runningRef.current || positionRef.current !== "left") {
@@ -172,7 +249,7 @@ const Home = () => {
           await runCollectorLeftPosition(i, j, gridCopy);
           setGrid(JSON.parse(JSON.stringify(gridCopy)));
         }
-        y = numCols - 1;
+        y = gridCols - 1;
       }
     } else if (!runningRef.current) {
       setCollectorPosition(positionRef.current);
@@ -182,12 +259,12 @@ const Home = () => {
   /* move collector down on the grid */
   const moveCollectorBottom = async () => {
     positionRef.current = "down";
-    if (runningRef.current && collectorCommand === "down") {
+    if (runningRef.current && collectorPosition === "down") {
       let gridCopy = JSON.parse(JSON.stringify(grid));
       /* traverse grid top to down */
       let y = collectorCoordinates.xCord;
-      for (let i = collectorCoordinates.yCord; i < numCols; i++) {
-        for (let j = y; j < numRows; j++) {
+      for (let i = collectorCoordinates.yCord; i < gridCols; i++) {
+        for (let j = y; j < gridRows; j++) {
           /* stop loop if running state change or direction changes  */
           if (!runningRef.current || positionRef.current !== "down") {
             setCollectorPosition(positionRef.current);
@@ -222,7 +299,7 @@ const Home = () => {
           await runCollectorTopPosition(i, j, gridCopy);
           setGrid(JSON.parse(JSON.stringify(gridCopy)));
         }
-        y = numCols - 1;
+        y = gridCols - 1;
       }
     } else if (!runningRef.current) {
       setCollectorPosition(positionRef.current);
@@ -234,16 +311,17 @@ const Home = () => {
     return new Promise(function (resolve) {
       timeOutId = setTimeout(function () {
         /* check boundary*/
-        if (i < 0 || j < 0 || i >= numRows || j >= numCols) {
+        if (i < 0 || j < 0 || i >= gridRows || j >= gridCols) {
         } else {
           if (j - 1 >= 0) {
             if (gridCopy[i][j - 1] !== 1) gridCopy[i][j - 1] = 0;
             /* change value of cell visited 1 to 0 i.e. mine picked */
             if (gridCopy[i][j] === 1) {
               gridCopy[i][j] = 0;
+              getMinesCount(gridCopy);
             }
           } else {
-            if (i - 1 >= 0) gridCopy[i - 1][numCols - 1] = 0;
+            if (i - 1 >= 0) gridCopy[i - 1][gridCols - 1] = 0;
           }
           gridCopy[i][j] = 2;
 
@@ -254,7 +332,7 @@ const Home = () => {
           });
         }
         resolve(gridCopy);
-      }, 1000);
+      }, collectorSpeed);
     });
   };
 
@@ -263,13 +341,15 @@ const Home = () => {
     return new Promise(function (resolve) {
       timeOutId = setTimeout(function () {
         /* check boundary*/
-        if (i < 0 || j < 0 || i >= numRows || j >= numCols) {
+        if (i < 0 || j < 0 || i >= gridRows || j >= gridCols) {
         } else {
-          if (j + 1 < numCols) {
+          if (j + 1 < gridCols) {
             if (gridCopy[i][j + 1] !== 1) gridCopy[i][j + 1] = 0;
             /* change value of cell visited 1 to 0 i.e. mine picked */
             if (gridCopy[i][j] === 1) {
               gridCopy[i][j] = 0;
+
+              getMinesCount(gridCopy);
             }
           } else if (i - 1 >= 0) {
             gridCopy[i - 1][0] = 0;
@@ -283,7 +363,7 @@ const Home = () => {
           });
         }
         resolve(gridCopy);
-      }, 1000);
+      }, collectorSpeed);
     });
   };
 
@@ -291,15 +371,16 @@ const Home = () => {
   const runCollectorTopPosition = (i, j, gridCopy) => {
     return new Promise(function (resolve) {
       timeOutId = setTimeout(function () {
-        if (i < 0 || j < 0 || i >= numCols || j >= numRows) {
+        if (i < 0 || j < 0 || i >= gridCols || j >= gridRows) {
         } else {
-          if (j + 1 < numRows) {
+          if (j + 1 < gridRows) {
             if (gridCopy[j + 1][i] !== 1) gridCopy[j + 1][i] = 0;
             /* change value of cell visited 1 to 0 i.e. mine picked */
             if (gridCopy[j][i] === 1) {
               gridCopy[j][i] = 0;
+              getMinesCount(gridCopy);
             }
-          } else if (i + 1 < numCols) {
+          } else if (i + 1 < gridCols) {
             gridCopy[0][i + 1] = 0;
           }
           gridCopy[j][i] = 2;
@@ -311,7 +392,7 @@ const Home = () => {
           });
         }
         resolve(gridCopy);
-      }, 1000);
+      }, collectorSpeed);
     });
   };
 
@@ -319,16 +400,17 @@ const Home = () => {
   const runCollectorBottomPosition = (i, j, gridCopy) => {
     return new Promise(function (resolve) {
       timeOutId = setTimeout(function () {
-        if (i < 0 || j < 0 || i >= numCols || j >= numRows) {
+        if (i < 0 || j < 0 || i >= gridCols || j >= gridRows) {
         } else {
           if (j - 1 >= 0) {
             if (gridCopy[j - 1][i] !== 1) gridCopy[j - 1][i] = 0;
             /* change value of cell visited 1 to 0 i.e. mine picked */
             if (gridCopy[j][i] === 1) {
               gridCopy[j][i] = 0;
+              getMinesCount(gridCopy);
             }
           } else {
-            if (i - 1 >= 0) gridCopy[numRows - 1][i - 1] = 0;
+            if (i - 1 >= 0) gridCopy[gridRows - 1][i - 1] = 0;
           }
           gridCopy[j][i] = 2;
           /* set updated position of collector */
@@ -338,10 +420,11 @@ const Home = () => {
           });
         }
         resolve(gridCopy);
-      }, 1000);
+      }, collectorSpeed);
     });
   };
 
+  /* connect to web socket */
   const connectWebsocket = async () => {
     const { websocketSendUrl, websocketReceiveUrl } = await fetchWebsocketUrl();
     startMicrophoneCapture(websocketSendUrl, websocketReceiveUrl);
@@ -355,6 +438,7 @@ const Home = () => {
     if (websocket === undefined) {
       const socket = new WebSocket(websocketReceiveUrl);
       socket.onopen = () => {
+        setConnection(true);
         console.log("Websocket is connected");
         socket.addEventListener("message", (event) => {
           let currentDate = new Date();
@@ -398,56 +482,143 @@ const Home = () => {
     }
   };
 
-  return (
-    <div>
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: `repeat(${numCols}, 42px)`,
-          padding: "10px",
-          width: "fit-content",
-          margin: "0 auto",
-        }}
-      >
-        {grid &&
-          grid.map((rows, i) =>
-            rows.map((col, k) => (
-              <div
-                key={`${i}-${k}`}
-                style={{
-                  border: "1px solid rgba(0, 0, 0, 0.8)",
-                  height: "30px",
-                  width: "30px",
-                  padding: "5px",
-                  border: "1px solid #595959",
-                }}
-              >
-                {grid[i][k] === 2 && getCollectorDirection()}
+  /* change grid size */
+  const onChangeGridSize = (e) => {
+    const number = Number(e.target.value);
+    setCollectorCoordinates({
+      xCord: 0,
+      yCord: 5,
+    });
+    setTimer("00:00:00");
+    scoreboardTime = {};
+    setGridRows(number);
+    setGridCols(number);
+  };
 
-                {grid[i][k] === 1 ? <span>&#9711;</span> : null}
-              </div>
-            ))
-          )}
+  /* change collector speed */
+  const onChangeCollectorSpeed = (e) => {
+    const speed = Number(e.target.value);
+    setCollectorSpeed(speed);
+  };
+
+  /* get total remaining mines  */
+  const getMinesCount = (gridCopy) => {
+    let mineCount = 0;
+    for (let i = 0; i < gridRows; i++) {
+      for (let j = 0; j < gridCols; j++) {
+        if (gridCopy[i][j] === 1) {
+          mineCount++;
+        }
+      }
+    }
+    if (mineCount === 0) {
+      setRunning(false);
+      if (timerRef.current) clearInterval(timerRef.current);
+      setScoreBoard();
+    }
+  };
+
+  /* set scoreboard with gridsize and time */
+  const setScoreBoard = () => {
+    const prevScoreBoard =
+      JSON.parse(localStorage.getItem("score")) || scoreBoard;
+    scoreBoard = [
+      ...prevScoreBoard,
+      {
+        gird: "" + gridRows + "*" + "" + gridCols,
+        time: scoreboardTime.time,
+        timestamp: scoreboardTime.timestamp,
+      },
+    ];
+    scoreBoard = scoreBoard.sort((a, b) => a.timestamp - b.timestamp);
+    setScore(scoreBoard);
+    localStorage.setItem("score", JSON.stringify(scoreBoard));
+  };
+
+  return (
+    <div style={{ display: "flex" }}>
+      <div className="gridSetting">
+        <label htmlFor="gridSize">Grid Size:</label>
+        <select
+          className="gridSizeOption"
+          name="gridSize"
+          disabled={running}
+          onChange={(e) => onChangeGridSize(e)}
+        >
+          <option value="10">10*10</option>
+          <option value="15">15*15</option>
+          <option value="20">20*20</option>
+        </select>
+        <label htmlFor="collectorSpeed">Collector Speed:</label>
+        <select
+          className="gridSizeOption"
+          name="collectorSpeed"
+          disabled={running}
+          onChange={(e) => onChangeCollectorSpeed(e)}
+        >
+          <option value="1000">1000 ms</option>
+          <option value="500">500 ms</option>
+          <option value="600">600 ms</option>
+        </select>
       </div>
-      <div className="btn-container">
-        <button className="button" onClick={() => setConnection(true)}>
-          <span>{"Connect"}</span>
-        </button>
-        <button className="button" onClick={() => startGame()}>
-          <span>{running ? "Stop" : "Start"}</span>
-        </button>
-        <button className="button" onClick={() => moveCollectorLeft()}>
-          <span>{"Left"}</span>
-        </button>
-        <button className="button" onClick={() => moveCollectorRight()}>
-          <span>{"Right"}</span>
-        </button>
-        <button className="button" onClick={() => moveCollectorTop()}>
-          <span>{"Up"}</span>
-        </button>
-        <button className="button" onClick={() => moveCollectorBottom()}>
-          <span>{"Down"}</span>
-        </button>
+      <div style={{ width: "60%" }}>
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: `repeat(${gridCols}, 42px)`,
+            padding: "10px",
+            width: "fit-content",
+            margin: "0 auto",
+          }}
+        >
+          {grid &&
+            grid.map((rows, i) =>
+              rows.map((col, k) => (
+                <div
+                  key={`${i}-${k}`}
+                  style={{
+                    border: "1px solid rgba(0, 0, 0, 0.8)",
+                    height: "30px",
+                    width: "30px",
+                    padding: "5px",
+                    border: "1px solid #595959",
+                  }}
+                >
+                  {grid[i][k] === 2 && getCollectorDirection()}
+
+                  {grid[i][k] === 1 ? <span>&#9711;</span> : null}
+                </div>
+              ))
+            )}
+        </div>
+        <div className="btn-container">
+          <button className="button" onClick={() => connectWebsocket()}>
+            <span>{connection ? "Connected" : "Connect"}</span>
+          </button>
+          <button className="button" onClick={() => restart()}>
+            <span>{"Restart"}</span>
+          </button>
+        </div>
+      </div>
+      <div className="scoreBoard">
+        <label>Timer</label> :{" "}
+        <span style={{ fontSize: "20px", fontWeight: "500" }}>{timer}</span>
+        <table>
+          <tbody>
+            <tr>
+              <th>Grid Size</th>
+              <th>Time</th>
+            </tr>
+            {score.map((val, key) => {
+              return (
+                <tr key={key}>
+                  <td>{val.gird}</td>
+                  <td>{val.time}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     </div>
   );
